b2c9f15c76de55d0bd09215e3d901491dbfc4d80
GradyHooker <bloodninja28@gmail.com>
2017-12-24 12:18:31 +1300

    Time conversion now works

--- js/main.js
+++ js/main.js
@@ -19,4 +19,27 @@ $(document).ready(function() {
 	}
 
 	//Rewrite times
+	var times = $('time');
+	var now = new Date($.now());
+	var time, utc1, utc2, diffDays, diffHours, diffMins, displayTime;
+	
+	times.each(function() {
+		displayTime = "";
+		time = new Date($(this).attr("datetime"));
+		utc1 = Date.UTC(time.getFullYear(), time.getMonth(), time.getDate(), time.getHours(), time.getMinutes(), time.getSeconds());
+		utc2 = Date.UTC(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes(), now.getSeconds());
+		
+		diffMins = Math.floor((utc2 - utc1) / 1000 / 60);
+		diffHours = Math.floor(diffMins / 60);
+		diffDays = Math.floor(diffHours / 24);
+		
+		if(diffMins == 1) displayTime = "a minute ago"
+		else if(diffMins <= 59) displayTime = diffMins + " minutes ago"
+		else if(diffHours == 1) displayTime = "an hour ago"
+		else if(diffHours <= 23) displayTime = diffHours + " hours ago"
+		else if(diffDays == 1) displayTime = "a day ago"
+		else if(diffDays <= 6) displayTime = diffDays + " days ago"
+		
+		if(displayTime != "") $(this).text(displayTime);
+	});
 });
