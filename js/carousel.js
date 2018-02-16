dfd4b5fa07c7dde0f84b31cae29b35137b4f625c
GradyHooker <bloodninja28@gmail.com>
2018-02-16 22:06:08 +1300

    Carousels can now have no captions and look different

--- js/carousel.js
+++ js/carousel.js
@@ -1,20 +1,51 @@
-$(".slides").slick({
-  asNavFor: '.captions',
-  infinite: false,
-  speed: 200,
-  arrows: false,
-  autoplay: true,
-  swipeToSlide: true,
-  infinite: true
-})
+$(".slides").each(function(index) {
+	console.log(index + " = " + $(this).data("builtin"));
+	$(this).addClass("slides-" + index);
+	console.log($(this));
+	$(this).parent().addClass("carousel-" + index);
+	console.log($(this).parent());
+	if($(this).data("builtin") == true) {
+		//No Captions
+		console.log("No Captions");
+		$(".slides-" + index).slick({
+		  infinite: false,
+		  speed: 200,
+		  arrows: true,
+		  autoplay: true,
+		  swipeToSlide: true,
+		  infinite: true,
+		  prevArrow: '<div class="pagination__button pagination__button_nocap"><</div>',
+		  nextArrow: '<div class="pagination__button pagination__button_nocap">></div>'
+		});
+	} else {
+		//With Captions
+		console.log("WITH Cpations");
+		$(this).parent().find(".captions").addClass("captions-" + index);
+		console.log($(this).parent().find(".captions"));
+		$(this).parent().find(".pagination").addClass("pagination-" + index);
+		console.log($(this).parent().find(".pagination"));
+		$(".slides-" + index).slick({
+		  asNavFor: '.captions-' + index,
+		  infinite: false,
+		  speed: 200,
+		  arrows: false,
+		  autoplay: true,
+		  swipeToSlide: true,
+		  infinite: true
+		})
 
-$(".captions").slick({
-  asNavFor: '.slides',
-  infinite: false,
-  speed: 200,
-  fade: true,
-  infinite: true,
-  appendArrows: $('.pagination'),
-  prevArrow: '<div class="pagination__button"><</div>',
-  nextArrow: '<div class="pagination__button">></div>'
-})
+		$(".captions-" + index).slick({
+		  asNavFor: '.slides-' + index,
+		  infinite: false,
+		  speed: 200,
+		  fade: true,
+		  infinite: true,
+		  appendArrows: $('.pagination-' + index),
+		  prevArrow: '<div class="pagination__button"><</div>',
+		  nextArrow: '<div class="pagination__button">></div>'
+		})
+	}
+});
+
+
+
