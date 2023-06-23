0b1793e755af8667199a6406c33191a5ca366960
gradyhooker <bloodninja28@gmail.com>
2023-06-23 19:40:30 +1200

    Fix Tweet embed

---
+++ script/cibuild.ps1
@@ -0,0 +1,39 @@
+cd ..
+rm -Force _site
+mkdir _site
+
+# clone remote repo to "_site"
+git clone --depth 1 https://github.com/GradyHooker/esportskingdom.git --branch gh-pages _site
+
+# back up the old medium and tiny images
+mkdir bak_files
+cp -r _site/assets/logos/medium bak_files
+rm -Force _site/assets/logos/medium
+cp -r _site/assets/logos/tiny bak_files
+rm -Force _site/assets/logos/tiny
+
+# build with Jekyll into "_site"
+bundle exec jekyll build
+
+# copy cname file
+cp CNAME _site
+
+# create new images
+mkdir _site/assets/logos/medium
+mkdir _site/assets/logos/tiny
+cd _site/assets/logos/original
+mogrify -path ../medium -resize 80x80 -format png *.png
+mogrify -path ../tiny -resize 24x24 -format png *.png
+cd ../../../../
+
+# copy back in any old ones we had
+cp -r bak_files/medium _site/assets/logos
+cp -r bak_files/tiny _site/assets/logos
+rm -Force bak_files
+
+# push
+cd _site
+git config user.name "Grady Bot"
+git add --all
+git commit -a -m "Grady Build"
+git push --force origin gh-pages
